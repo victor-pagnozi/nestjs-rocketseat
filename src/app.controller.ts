22995b0247df50214309a5cd1aa03af1d50197d2
Victor Pagnozi <victor.pagnozi@gmail.com>
2023-01-21 18:15:36 -0300

    Strated function to save and request decorators in nestjs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 import { AppService } from './app.service';
+import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './prisma.service';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return this.prismaService.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { category, content, recipientId } = body;
+
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        category,
+        content,
+        recipientId,
+      },
+    });
   }
 }
